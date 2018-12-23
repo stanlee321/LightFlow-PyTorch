822e1f7ea8c92a13fd992a162fd827cfd1eb0d52
stanlee321 <stanlee321@gmail.com>
2018-12-23 19:15:40 -0400

    updated download link

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+DIRECTORY=data
+if [ ! -d "$DIRECTORY" ]; then
+  echo Dir exist...
+fi
 mkdir data
 cd data
-wget http://sintel.cs.washington.edu/MPI-Sintel-complete.zip
+wget http://files.is.tue.mpg.de/sintel/MPI-Sintel-complete.zip
 unzip MPI-Sintel-complete.zip -d MPI-Sintel
 
 
